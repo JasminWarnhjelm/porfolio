c8317199b0b9c367b4cf8e9d28bae80bb00a40fb
JasminWarnhjelm <jasmin.warnhjelm@tuni.fi>
2025-01-24 16:18:57 +0200

    Delete doc directory

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Choices"},{"p":"<Unnamed>","l":"ContactsApp"},{"p":"<Unnamed>","l":"MyFile"},{"p":"<Unnamed>","l":"Validation"}];updateSearchResults();
